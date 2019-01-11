2c0c0a8edfa3e6a50f78406cc05499b85a4084c4
Conrad MacKethan <cmackethan@students.stab.org>
2019-01-11 09:31:49 -0500

    update to openCV variables

--- mussel.js
+++ mussel.js
@@ -4,7 +4,7 @@
 //add express, node.js, opencv library, and/or synaptic.js if we need it\
 
 //some openCV framework
-let imgElement = document.getElementById("imageSrc")
+let imgElement = document.getElementById("imageInput")
 let inputElement = document.getElementById("fileInput");
 inputElement.addEventListener("change", (e) => {
   imgElement.src = URL.createObjectURL(e.target.files[0]);
