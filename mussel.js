80a1406fb33e25cd6ebf8af730073c0808e168a0
Conrad MacKethan <cmackethan@students.stab.org>
2019-01-11 09:22:52 -0500

    changes to UI

--- mussel.js
+++ mussel.js
@@ -2,3 +2,9 @@
 
 //where the mussel algorithm should be...
 //add express, node.js, opencv library, and/or synaptic.js if we need it
+
+let imgElement = document.getElementById("imageSrc")
+let inputElement = document.getElementById("fileInput");
+inputElement.addEventListener("change", (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
