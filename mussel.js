c2f2121356f603bc471c2a045c4fa75789eed62c
Conrad MacKethan <cmackethan@students.stab.org>
2019-01-22 13:45:24 -0500

    Fixed merge

--- mussel.js
+++ mussel.js
@@ -4,8 +4,17 @@
 //add express, node.js, opencv library, and/or synaptic.js if we need it\
 
 //some openCV framework
-let imgElement = document.getElementById("imageInput")
-let inputElement = document.getElementById("fileInput");
+let imgElement = document.getElementById("imageinput")
+let inputElement = document.getElementById("submitbutton");
 inputElement.addEventListener("change", (e) => {
   imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
+
+imgElement.onload = function() {
+  let mat = cv.imread(imgElement);
+  cv.imshow('canvasOutput', mat);
+  mat.delete();
+};
+function onOpenCvReady() {
+  document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+}
