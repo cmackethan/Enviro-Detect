2aaa2fcf6f52756ee2eed28d874273233cf17d4f
Conrad MacKethan <cmackethan@students.stab.org>
2019-01-23 09:37:20 -0500

    stuff

--- mussel.js
+++ mussel.js
@@ -10,11 +10,3 @@ inputElement.addEventListener("change", (e) => {
   imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
-imgElement.onload = function() {
-  let mat = cv.imread(imgElement);
-  cv.imshow('canvasOutput', mat);
-  mat.delete();
-};
-function onOpenCvReady() {
-  document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
-}
